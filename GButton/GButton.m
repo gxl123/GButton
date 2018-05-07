37297aa974a7570d1f8000836849637eb42740a5
gxl <2180877579@qq.com>
2018-05-07 15:24:27 +0800

    update code

--- GButton/GButton.m
+++ GButton/GButton.m
@@ -10,12 +10,25 @@
 
 @implementation GButton
 
-/*
-// Only override drawRect: if you perform custom drawing.
-// An empty implementation adversely affects performance during animation.
-- (void)drawRect:(CGRect)rect {
-    // Drawing code
+- (instancetype)init{
+    self = [super init];
+    if (self) {
+        
+        self.backgroundColor = [UIColor whiteColor];
+        [self setTitleColor:[UIColor blueColor] forState:UIControlStateNormal];
+        self.layer.cornerRadius = 5;
+        self.layer.shadowOffset =  CGSizeMake(2, 2);
+        self.layer.shadowOpacity = 0.2;
+        self.layer.borderColor = [UIColor blueColor].CGColor;
+        self.layer.borderWidth = 2;
+        self.layer.shadowColor = [UIColor blackColor].CGColor;//[UIColor c9_Color].CGColor;
+    }
+    return self;
 }
-*/
+
+- (void)setSelected:(BOOL)selected{
+    [super setSelected:selected];
+    self.alpha = selected ? 1:0.5;
+}
 
 @end
