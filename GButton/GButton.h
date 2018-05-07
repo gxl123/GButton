593ec5c6d30153c20d837473fdd6ff17bfc414c7
gxl <2180877579@qq.com>
2018-05-07 15:24:00 +0800

    加入code

---
+++ GButton/GButton.h
@@ -0,0 +1,13 @@
+//
+//  GButton.h
+//  testGButton
+//
+//  Created by gxl on 2018/5/7.
+//  Copyright © 2018年 gxl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GButton : UIButton
+
+@end
---
+++ testGButton/testGButton/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  testGButton
+//
+//  Created by gxl on 2018/5/7.
+//  Copyright © 2018年 gxl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
